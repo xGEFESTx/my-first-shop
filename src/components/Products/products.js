5b925f99bb4a82e5d74e9c7c28c67faca646cdbe
shgen <sh.genka@gmail.com>
2020-12-22 01:45:47 +0200

    finish component catalog prodacts

--- src/components/Products/products.js
+++ src/components/Products/products.js
@@ -1,9 +1,24 @@
 class Products{
 
     render(){
+        let htmlCatalog = '';
         CATALOG.forEach( ({id, name, price, img}) => { 
-            console.log(id, name, price, img);
-        })
+                htmlCatalog += `
+                <li>
+                    <span>${name}</span>
+                    <img src="${img}"/>
+                    <span>${price}</span>
+                    <button>Добавить в корзину</button>
+                </li>
+                `;
+        });
+        const html = `
+        <ul>
+            ${htmlCatalog}
+        </ul>
+        `;
+
+        ROOT_PRODUCTS.innerHTML = html;
     }
 }
 
